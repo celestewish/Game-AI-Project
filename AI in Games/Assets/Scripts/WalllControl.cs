6d5f537bd5ef24aacc3a56f1174b31b869ce638a
lismar17 <lisbeth.mtzj@gmail.com>
2025-11-30 15:06:14 -0500

    Random Wall Generator

---
+++ AI in Games/Assets/Scripts/WalllControl.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class WalllControl : MonoBehaviour
+{
+    public int randomAngle;
+    public int randomNum;
+    public int min = 0;
+    public int max = 180;
+    void Start()
+    {
+        WallVisible();
+        randomAngle = Random.Range(min, max);
+        //random wall y position at game start   
+        transform.rotation = Quaternion.Euler(new Vector3(0, randomAngle, 0));
+    }
+    public void WallVisible()
+    {
+        randomNum = Random.Range(min, max);
+        //if even set false
+        if (randomNum % 2 == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+
+    }
+}
